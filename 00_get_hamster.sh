8875d29fc90c7b64665eed632999984149740ebe
Jessica Keune <jessica.keune@ugent.be>
2021-12-20 12:55:05 +0100

    docs: note on v1.2.0

--- 00_get_hamster.sh
+++ 00_get_hamster.sh
@@ -2,6 +2,8 @@
 
 # clone latest version of hamster
 git clone https://github.com/h-cel/hamster
+# optional: additionally swap to v1.2.0 
+# git clone --branch v1.2.0 https://github.com/h-cel/hamster
 
 # move paths_*.txt and other files needed to run hamster to hamster/src directory
 mv hamster_supplement/* hamster/src/.
